6aa2d4e8e691b911a2fa53071a05bdbae09c7b9a
UlysseLBT <lebouteiller.ulysse@gmail.com>
2025-06-30 11:10:43 +0200

    Ajout de la migration cp_ville

---
+++ database/migrations/2025_06_30_090425_cp_ville.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('cp_ville', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('code_postal', 10);
+            $table->string('ville', 100);
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('cp_ville');
+    }
+};
